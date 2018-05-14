1509c4f83cd063109e3a44e962482571283bbfd6
Marlon <martianworm17@gmail.com>
2018-05-14 12:46:09 +0800

    Implement Send class

--- com.kamotelabs.rabbitmq/src/main/java/com/kamotelabs/rabbitmq/Send.java
+++ com.kamotelabs.rabbitmq/src/main/java/com/kamotelabs/rabbitmq/Send.java
@@ -1,5 +1,36 @@
 package com.kamotelabs.rabbitmq;
 
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+
+
 public class Send {
+	
+	private static final String QUEUE_NAME = "hello";
+	private static final String HOST_NAME = "localhost";
+	
+	public static void main(String[] args) throws IOException, TimeoutException {
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(HOST_NAME);
+		
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		String message = "Hello World!";
+		
+		channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+		System.out.println(" [x] Sent '" + message + "'");
+		
+		channel.close();
+		connection.close();
+		
+	}
 
 }
