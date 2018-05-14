0a032021e3654059a71dbbfb0ca49a9f0468b648
Marlon <martianworm17@gmail.com>
2018-05-14 14:54:24 +0800

    Implement Recv class

--- com.kamotelabs.rabbitmq/src/main/java/com/kamotelabs/rabbitmq/Recv.java
+++ com.kamotelabs.rabbitmq/src/main/java/com/kamotelabs/rabbitmq/Recv.java
@@ -1,5 +1,46 @@
 package com.kamotelabs.rabbitmq;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
 public class Recv {
+	
+	private static final String QUEUE_NAME = "hello";
+	private static final String HOST_NAME = "localhost";
+	
+	public static void main(String[] args) throws IOException, InterruptedException, TimeoutException {
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(HOST_NAME);
+		
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+	
+	
+		Consumer consumer = new DefaultConsumer(channel) {
+			
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope,
+					BasicProperties properties, byte[] body) throws IOException {
+			
+				String message = new String(body, "UTF-8");
+				System.out.println(" [x] Received '" + message + "'");
+			}
+		};
+		
+		channel.basicConsume(QUEUE_NAME, true, consumer);
+	}
 
 }
